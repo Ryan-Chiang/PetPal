c560847b45b75cc34ba5f90fc7ef7e079f86d0e5
Ryan <ryanjiang@outlook.com>
2024-09-26 08:16:19 +0000

    init the robot, install the referenced module, create the folder

---
+++ main.py
@@ -0,0 +1,66 @@
+import logging
+import asyncio, os, discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+# intents是要求機器人的權限
+intents = discord.Intents.all()
+# 前缀符号
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+# 載入.env檔案
+load_dotenv()
+token = os.getenv("TOKEN")
+print(token)
+
+
+@bot.event
+# 當機器人完成啟動
+async def on_ready():
+    print(f"当前登录身份 --> {bot.user}")
+    botCount = await bot.tree.sync()
+    for cmdItem in botCount:
+        print(f"sync cmd: {cmdItem.name}")
+
+
+
+#load commands
+@bot.command()
+async def load(ctx, extension):
+    await bot.load_extension(f"cogs.{extension}")
+    await ctx.send(f"Loaded {extension} done.")
+
+
+@bot.command()
+async def unload(ctx, extention):
+    await bot.unload_extension(f"cogs.{extention}")
+    await ctx.send(f"Unloaded {extention} done.")
+
+
+@bot.command()
+async def reload(ctx, extention):
+    await bot.reload_extension(f"cogs.{extention}")
+    await ctx.send(f"Reloaded {extention} done.")
+
+
+# 手动刷新命令提示
+@bot.command()
+async def refreshtree(ctx, extention):
+    await bot.tree.sync()
+
+
+async def load_extensions():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+            print(f'Loaded {filename[:-3]}')
+
+
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(token)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
