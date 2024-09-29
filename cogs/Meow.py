7fd4783438aaec7e1eeb2762382c57b6fe9e1f3c
Ryan <ryanjiang@outlook.com>
2024-09-29 07:45:10 +0000

    For the purpose of the new "recommend by conditions" feature, use the local method again，cause the oneline api don't support the condition search like more than 3 stars

--- cogs/Meow.py
+++ cogs/Meow.py
@@ -1,5 +1,5 @@
-import discord,os
-from dotenv import load_dotenv
+import json
+import discord
 import requests
 import random
 
@@ -10,29 +10,17 @@ from discord.ext import commands
 class Meow(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         super().__init__()
-
+    
     @app_commands.command(name="meow",description="recommand a cat randomly")
-    async def meow(self, interaction: discord.Interaction):
-
-        load_dotenv()
-        ninjia_key = os.getenv("X_Api_Key")
-        offset_random = random.randint(0, 81)
-
-        query_string = f'min_weight=1&offset={offset_random}'
-        res = requests.get(f"https://api.api-ninjas.com/v1/cats?{query_string}", headers={'X-Api-Key':f'{ninjia_key}'})
-        print(res.json())
-        if res.status_code == 200:            
-            idx_random = random.randint(0, len(res.json())-1)
-
-            item_random = res.json()[idx_random]
-
-
-            print(f'item_random[name]={item_random["name"]}')
-            # 初始化Embed，其中：
-            # 标题 = 猫名
-            # 颜色 = PetPal的主题色
+    async def meow(self, interaction: discord.Interaction):        
+        with open("resources/data/cat.json", "r") as f:
+            cat_data = json.load(f)
+            
+            idx_random = random.randint(0, len(cat_data)-1)
+            item_random = cat_data[idx_random]
+            
             item_embed = discord.Embed(title=item_random['name'], color=discord.Color.from_rgb(205,108,61))
-
+            
             # 作者 = PetPal
             # 图标 = PetPal的图标
             img_author_icon = discord.File('img/petpal_logo.png', filename='petpal_logo.png')
@@ -55,20 +43,20 @@ class Meow(commands.Cog):
             item_embed.add_field(name='grooming', value=to_star(item_random['grooming']), inline=True)
             item_embed.add_field(name='intelligence', value=to_star(item_random['intelligence']), inline=True)
             item_embed.add_field(name='other pets friendly', value=to_star(item_random['other_pets_friendly']), inline=True)
+            
+            
 
+            # 添加图片 = 猫的图片            
+            img_cat = discord.File(f"img/cats/{item_random['name']}.jpg", filename=f'{item_random['name']}.jpg')            
+            item_embed.set_image(url=f'attachment://{item_random['name']}.jpg')
 
-
-            # 添加图片 = 猫的图片
-            item_embed.set_image(url=item_random['image_link'])
-            print(item_random)
-            print(f'item_embed: {item_embed}')                       
+            # 发送Embed消息  
             try: 
-                await interaction.response.send_message(file=img_author_icon,embed=item_embed)
+                await interaction.response.send_message(files=[img_cat,img_author_icon],embed=item_embed)
             except Exception as e:
                 print(f'error: {e}')
-        else:
-            await interaction.response.send_message(f'something is wrong')
 
+            
 
 def to_star(num: int):
     try:
@@ -80,3 +68,4 @@ def to_star(num: int):
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Meow(bot))
+        
